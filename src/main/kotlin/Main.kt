2f09525317565afb3484b08d89707830524dbd04
Nessy01 <72563733+Nessy01@users.noreply.github.com>
2022-12-15 13:30:54 +0100

    first coroutines

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,18 @@
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 
-fun main() = runBlocking { // this: CoroutineScope
-    launch { // launch a new coroutine and continue
-        delay(1000L) // non-blocking delay for 1 second (default time unit is ms)
-        println("World!") // print after delay
+fun main() {
+    runBlocking{ //umozliwia wykonanie korutyny w biezacym watku, jeden watek moze wykonywac wiele korutyn
+        
+        launch {
+            println("Start #1 on thread ${Thread.currentThread().name}")
+            yield() // suspension point
+            println("Exit #1 on thread ${Thread.currentThread().name}")
+        }
+        launch {
+            delay(1000) // czeka sekunde
+            println("Start #2 on thread ${Thread.currentThread().name}")
+            println("Exit #2 on thread ${Thread.currentThread().name}")
+        }
     }
-    println("Hello") // main coroutine continues while a previous one is delayed
+
 }

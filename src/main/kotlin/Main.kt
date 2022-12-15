8e5089f5d398363c50f748534e9f8665edf08001
H A L T <72382258+Kamil097@users.noreply.github.com>
2022-12-15 15:20:03 +0100

    MainUpdate

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,13 @@
 import kotlinx.coroutines.*
+import kotlin.system.measureTimeMillis
 
 fun main() {
+    //example1()
+    //example2()
+    example3()
+}
+fun example1(){
     runBlocking{ //umozliwia wykonanie korutyny w biezacym watku, jeden watek moze wykonywac wiele korutyn
-//        val job = launch {
-//            println("Starting")
-//            delay(1000) // czeka sekunde
-//            println("Exiting") //nie wyswietli sie bo korutyna zostala anulowana job.cancel
-//        }
-//        delay(500)
-//        job.cancel()
-        //dispatcher
-        launch(Dispatchers.IO) {//dispatcher określa wątek wykonujący korutynę
-            println("A coroutine on thread ${Thread.currentThread().name}")
-        }
-        // pokazanie punktu wstrzymania
         val job = launch {
             println("Start #1 on thread ${Thread.currentThread().name}")
             yield() // suspension point
@@ -21,15 +15,46 @@ fun main() {
             delay(1000) // czeka sekunde
             println("XD")
         }
-//        job.cancel() // do zaprezentowania jak canceluje
         launch {
             println("Start #2 on thread ${Thread.currentThread().name}")
             println("Exit #2 on thread ${Thread.currentThread().name}")
         }
-//        Wykonana została pierwsza instrukcja z pierwszej korutyny.
-//        Korutyna w punkcie wstrzymania zostaje wstrzymana.
-//        Druga korutyna zostaje w całości wykonana.
-//        Wstrzymana wcześniej korutyna zostaje wznowiona.
     }
+    GlobalScope.launch{//zamist runBlocking
+            println("KOLEJNY TASK PO WSZYSTKICH INNYCH")
+    }
+}
+fun example2() {
+    runBlocking {
+        val job = launch {
+            println("Start #1 on thread ${Thread.currentThread().name}")
+            //yield() // suspension point
+            println("Exit #1 on thread ${Thread.currentThread().name}")
+            delay(1000) // czeka sekunde
+            println("MESSAGE NOT VIEWED")
+        }
+        delay(500)
+        job.cancel() // do zaprezentowania jak canceluje
+    }
+}
+fun example3(){
+    runBlocking {
+        launch {
+            val time = measureTimeMillis {
+                val one = async { doSomethingUsefulOne() }
+                val two = async { doSomethingUsefulTwo() }
+                println("The answer is ${one.await() + two.await()}")
+            }
+            println("Completed in $time ms")
+        }
+    }
+}
+suspend fun doSomethingUsefulOne(): Int {
+    delay(1000L) // pretend we are doing something useful here
+    return 13
+}
 
+suspend fun doSomethingUsefulTwo(): Int {
+    delay(1000L) // pretend we are doing something useful here, too
+    return 29
 }

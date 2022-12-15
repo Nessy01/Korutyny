9aaee07acf6ffbbbaddbc4fe53d2084aef77c7e8
Nessy01 <72563733+Nessy01@users.noreply.github.com>
2022-12-15 13:47:52 +0100

    update main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,17 +2,34 @@ import kotlinx.coroutines.*
 
 fun main() {
     runBlocking{ //umozliwia wykonanie korutyny w biezacym watku, jeden watek moze wykonywac wiele korutyn
-        
-        launch {
+//        val job = launch {
+//            println("Starting")
+//            delay(1000) // czeka sekunde
+//            println("Exiting") //nie wyswietli sie bo korutyna zostala anulowana job.cancel
+//        }
+//        delay(500)
+//        job.cancel()
+        //dispatcher
+        launch(Dispatchers.IO) {//dispatcher określa wątek wykonujący korutynę
+            println("A coroutine on thread ${Thread.currentThread().name}")
+        }
+        // pokazanie punktu wstrzymania
+        val job = launch {
             println("Start #1 on thread ${Thread.currentThread().name}")
             yield() // suspension point
             println("Exit #1 on thread ${Thread.currentThread().name}")
-        }
-        launch {
             delay(1000) // czeka sekunde
+            println("XD")
+        }
+//        job.cancel() // do zaprezentowania jak canceluje
+        launch {
             println("Start #2 on thread ${Thread.currentThread().name}")
             println("Exit #2 on thread ${Thread.currentThread().name}")
         }
+//        Wykonana została pierwsza instrukcja z pierwszej korutyny.
+//        Korutyna w punkcie wstrzymania zostaje wstrzymana.
+//        Druga korutyna zostaje w całości wykonana.
+//        Wstrzymana wcześniej korutyna zostaje wznowiona.
     }
 
 }

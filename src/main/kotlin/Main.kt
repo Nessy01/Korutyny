43da2c47b9b5fbd07efc0608de39004618710283
rafaello166 <rafaello166@interia.pl>
2022-12-19 00:13:53 +0100

    reformat

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,13 +3,11 @@ import java.awt.font.TextMeasurer
 import kotlin.system.measureTimeMillis
 
 fun main() {
-    example5()
-    //example2()
-    //example3()
+    example6()
 }
-fun example1(){
 
-    runBlocking{ //umozliwia wykonanie korutyny w biezacym watku, jeden watek moze wykonywac wiele korutyn
+fun example1() {
+    runBlocking { // umozliwia wykonanie korutyny w biezacym watku, jeden watek moze wykonywac wiele korutyn
         val job = launch {
             println("Start #1 on thread ${Thread.currentThread().name}")
             yield() // suspension point
@@ -22,17 +20,16 @@ fun example1(){
             println("Exit #2 on thread ${Thread.currentThread().name}")
         }
     }
-    GlobalScope.launch{//zamist runBlocking
+    GlobalScope.launch {//zamist runBlocking
         println("KOLEJNY TASK PO WSZYSTKICH INNYCH")
     }
 }
-fun example2(){
 
-    GlobalScope.launch{
+fun example2() {
+    GlobalScope.launch {
         delay(1000)
         println("GLOBAL SCOPE")
     }
-
 
     runBlocking {
 
@@ -44,18 +41,19 @@ fun example2(){
             delay(2000)
             println("SECOND MESSAGE")
         }
-        launch{
+        launch {
             delay(100)
             println("THIRD MESSAGE")
         }
-        async{
+        async {
             println("Hello there")
         }
 
     }
-    println("RUNBLOCKING HAS ENDED")
 
+    println("RUNBLOCKING HAS ENDED")
 }
+
 fun example3() {
     runBlocking {
         val job = launch {
@@ -67,67 +65,75 @@ fun example3() {
         job.cancel() // cancells job coroutine
     }
 }
-fun example4(){
-    //wszystko wykonuje się asynchronicznie
-    //na początku wykona się "FIRST PRINT" ponieważ najszybciej będzie trwać jego wykonanie,
-    //następnie odpala się korutyna, w której asynchronicznie wykonują się dane funkcje, a więc całość będzie trwać tyle,
-    //co najdłuższa funkcja w korutynie, a więc 4 sekundy. Jednocześnie asynchronicznie wykonuje się funkcja delay(4100)
-    //a więc praktycznie zaraz po poprzedniej korutynie delay przestaje działać i od razu wykonują się dalsze instrukcje
-    //które trwają około kolejnych 4 sekund.
-    //całość programu zatem trwa ok 8 sekund.
-    //EKSPERYMENT: Zmniejsz delay do 1 sekundy i zgadnij co się stanie
-    val continuum = measureTimeMillis {
-    runBlocking {
 
-        println("FIRST PRINT")
+/** Wszystko wykonuje się asynchronicznie
+ * na początku wykona się "FIRST PRINT" ponieważ najszybciej będzie trwać jego wykonanie,
+ * następnie odpala się korutyna, w której asynchronicznie wykonują się dane funkcje, a więc całość będzie trwać tyle,
+ * co najdłuższa funkcja w korutynie, a więc 4 sekundy. Jednocześnie asynchronicznie wykonuje się funkcja delay(4100)
+ * a więc praktycznie zaraz po poprzedniej korutynie delay przestaje działać i od razu wykonują się dalsze instrukcje
+ * które trwają około kolejnych 4 sekund.
+ * całość programu zatem trwa ok 8 sekund.
+ * EKSPERYMENT: Zmniejsz delay do 1 sekundy i zgadnij co się stanie
+ */
+fun example4() {
+    val continuum = measureTimeMillis {
+        runBlocking {
+
+            println("FIRST PRINT")
+            launch {
+                val time = measureTimeMillis {
+                    val one = async { doSomethingUsefulOne() }
+                    val two = async { doSomethingUsefulTwo() }
+                    val three = async { doSomethingUsefulTwo() }
+                    println("The answer is ${one.await() + two.await() + three.await()}")
+                }
+                println("Completed in $time ms")
+            }
+
+            launch {
+                val time2 = measureTimeMillis {
+                    delay(4100)
+                    println(doSomethingUsefulOne())
+                }
+                println("Part 2 lasted for $time2")
+            }
+        }
+    }
+    println("WHOLE PROGRAM completed in $continuum ms")
+}
+
+fun example5() {
+    runBlocking {
         launch {
             val time = measureTimeMillis {
-                val one = async { doSomethingUsefulOne() }
-                val two = async { doSomethingUsefulTwo() }
-                val three = async { doSomethingUsefulTwo() }
-                println("The answer is ${one.await() + two.await() + three.await()}")
-            }
-            println("Completed in $time ms")
-        }
-
-        launch {
-            val time2 = measureTimeMillis {
+                println("FIRST PRINT")
+                val one = doSomethingUsefulOne()
+                val two = doSomethingUsefulTwo()
+                val three = doSomethingUsefulTwo()
+                println("The answer is: ${one + two + three}")
                 delay(4100)
                 println(doSomethingUsefulOne())
             }
-            println("Part 2 lasted for $time2")
+            println("ended in $time")
         }
-    } }
-    println("WHOLE PROGRAM completed in $continuum ms")
-}
-fun example5(){
-runBlocking {
-    launch {
-        val time = measureTimeMillis {
-            println("FIRST PRINT")
-            val one = doSomethingUsefulOne()
-            val two = doSomethingUsefulTwo()
-            val three = doSomethingUsefulTwo()
-            println("The answer is: ${one + two + three}")
-            delay(4100)
-            println(doSomethingUsefulOne())
-        }
-        println("ended in $time")
     }
-} }
+}
+
 suspend fun doSomethingUsefulOne(): Int {
     delay(4000L) // pretend we are doing something useful here
     return 13
 }
+
 suspend fun doSomethingUsefulTwo(): Int {
     delay(1500L) // pretend we are doing something useful here, too
     return 29
 }
-//CORUTINE DISPATCHERS - opisują w jakich wątków korutyna używa
-//Dzięki nim można ograniczyć wykonanie korutyny do danego wątku, wysłać je do puli wątków lub pozwolić działać bez ograniczeń
-//Launch bez parametrów dziedziczy kontekst ( a więc dispatcher ) z runBlocking korutyny, która działa w głównym wątku
-// I tak dalej, nie jestem pewien czy to będzie jakieś giga ważne
-fun example6(){
+
+/** CORUTINE DISPATCHERS - opisują w jakich wątków korutyna używa
+ * Dzięki nim można ograniczyć wykonanie korutyny do danego wątku, wysłać je do puli wątków lub pozwolić działać bez ograniczeń
+ * Launch bez parametrów dziedziczy kontekst ( a więc dispatcher ) z runBlocking korutyny, która działa w głównym wątku
+ */
+fun example6() {
     runBlocking {
         launch { // context of the parent, main runBlocking coroutine
             println("main runBlocking      : I'm working in thread ${Thread.currentThread().name}")
